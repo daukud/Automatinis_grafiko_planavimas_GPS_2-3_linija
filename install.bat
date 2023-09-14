2c08c2a40810a69a3a9de896f8766b6067d63576
daukud <73535207+daukud@users.noreply.github.com>
2023-09-14 15:04:06 +0300

    Delete install.bat

--- install.bat
+++
@@ -1,7 +0,0 @@
-rem backup
-
-xcopy /y /f "\ma1\Gamybos_padalinys\Gamybos_planavimo_skyrius\Automatinis grafiko planavimas 2-3 linija\Planas 2-3 linija 37 sav DERINIMUI.xlsm" "\ma1\Gamybos_padalinys\Gamybos_planavimo_skyrius\Automatinis grafiko planavimas 2-3 linija\archyvas\2003\Planas 2-3 linija 37 sav DERINIMUI.xlsm"
-
-rem copy
-
-xcopy /y /f "Planas 2-3 linija 37 sav DERINIMUI TEST.xlsm" "\ma1\Gamybos_padalinys\Gamybos_planavimo_skyrius\Automatinis grafiko planavimas 2-3 linija\Planas 2-3 linija 37 sav DERINIMUI.xlsm"
